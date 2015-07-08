e564fa9d2f48afdbe3f0f4d52e51bdcb94330642
Sym Roe <sym.roe@talusdesign.co.uk>
2015-07-08 14:09:46 +0100

    Part/Full time on latest jobs

--- jobs/jobs.rb
+++ jobs/jobs.rb
@@ -5,11 +5,18 @@ SCHEDULER.every '10m', :first_in => 0 do
   response = Net::HTTP.get_response(URI('https://lm-tools-jobs-api.herokuapp.com/api/jobs_in_area/?format=json'))
   job_hashes = JSON.parse(response.body)
   jobs = job_hashes.map do |job_hash|
+    
+    display_labels = {
+      "full_time" => "Full Time",
+      "part_time" => "Part Time"
+    }
+
     {
       job_title: job_hash["job_title"],
       company: job_hash["company"]["display_name"],
-      created: DateTime.parse(job_hash["created"]).strftime("%e %b %Y %H:%M:%S%p"),
-      category: job_hash["job_category"]
+      created: Time.parse(job_hash["created"]).ago_in_words,
+      category: job_hash["job_category"],
+      contract_time: display_labels.fetch(job_hash["contract_time"], job_hash["contract_time"])
     }
   end
   send_event('jobs', { title: "Latest jobs", items: jobs })
